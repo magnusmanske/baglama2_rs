c13ae22a26453861937aeab56a038d7757583080
Magnus Manske <magnusmanske@googlemail.com>
2024-11-28 13:00:22 +0000

    misc

--- restart.sh
+++ restart.sh
@@ -4,5 +4,6 @@ toolforge jobs delete rustbot
 toolforge jobs run --mem 4000Mi --cpu 3 --continuous --mount=all \
 	--image glamtools/glamtools:latest \
 	--command "sh -c 'target/release/baglama2 next_all'" \
+	--schedule "17 3 2 * *" \
 	--filelog -o /data/project/glamtools/rustbot.out -e /data/project/glamtools/rustbot.err \
 	rustbot
