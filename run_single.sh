b2509abb4f08ca3e014b6f722b55c89bd5294105
Magnus Manske <magnusmanske@googlemail.com>
2024-11-28 11:27:06 +0000

    misc

--- run_single.sh
+++ run_single.sh
@@ -3,8 +3,5 @@ toolforge jobs delete single 2> /dev/null
 
 rm ~/single.*
 
-toolforge jobs run --wait --mem 3000Mi --cpu 3 --mount=all --filelog --image tool-glamtools/tool-glamtools:latest \
+toolforge jobs run --mem 3000Mi --cpu 3 --mount=all --filelog --image tool-glamtools/tool-glamtools:latest \
 --command "sh -c 'target/release/baglama2 $1 $2 $3 $4'" single
-
-#toolforge jobs logs single
-cat ~/single.err
