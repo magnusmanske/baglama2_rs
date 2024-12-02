64584d9acd8d285dc209a35901cbf9ada9dd893e
Magnus Manske <magnusmanske@googlemail.com>
2024-12-02 08:54:36 +0000

    misc

--- restart.sh
+++ restart.sh
@@ -6,7 +6,7 @@ toolforge jobs run \
 	--cpu 3 \
 	--mount=all \
 	--image tool-glamtools/tool-glamtools:latest \
-	--command "sh -c 'target/release/baglama2 next_all'" \
+	--command "sh -c 'target/release/baglama2 next_all lm lm'" \
 	--schedule "17 3 2 * *" \
 	--filelog -o /data/project/glamtools/rustbot.out -e /data/project/glamtools/rustbot.err \
 	rustbot
