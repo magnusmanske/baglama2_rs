87f6ff7733a295eaf49fca5b0519a2280c59ce56
Magnus Manske <magnusmanske@googlemail.com>
2024-12-05 13:34:13 +0000

    misc

--- run_single.sh
+++ run_single.sh
@@ -4,4 +4,4 @@ toolforge jobs delete single 2> /dev/null
 rm ~/single.*
 
 toolforge jobs run --mem 3000Mi --cpu 3 --mount=all --filelog --image tool-glamtools/tool-glamtools:latest \
---command "sh -c 'target/release/baglama2 $1 $2 $3 $4'" single
+--command "sh -c 'RUST_BACKTRACE=1 target/release/baglama2 $1 $2 $3 $4'" single
