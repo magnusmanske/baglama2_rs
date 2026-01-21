b02b29d9ea44a359945cdbed020c6eb6f60fbde8
Magnus Manske <magnusmanske@googlemail.com>
2026-01-21 17:53:48 +0000

    misc

---
+++ run_views.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# parameters: year month
+toolforge jobs delete single 2> /dev/null
+
+rm ~/views.*
+
+toolforge jobs run --mem 500Mi --retry 5 --mount=all --filelog --image tool-glamtools/tool-glamtools:latest \
+--command "sh -c 'RUST_BACKTRACE=1 target/release/baglama2 mysql2_views $1 $2'" views
