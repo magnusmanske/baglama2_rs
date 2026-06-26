79e7034af88477447ff8b3d6f7fb911487c639b7
Magnus Manske <magnusmanske@googlemail.com>
2026-06-26 14:32:09 +0100

    Create connect_db.sh

---
+++ connect_db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#ssh magnus@login.toolforge.org -L 3309:wikidatawiki.web.db.svc.eqiad.wmflabs:3306 -N &
+ssh magnus@login.toolforge.org -L 3377:commonswiki.web.db.svc.eqiad.wmflabs:3306 -N &
+#ssh magnus@login.toolforge.org -L 3308:tools-db:3306 -N &
+#ssh magnus@login.toolforge.org -L 3317:termstore.wikidatawiki.analytics.db.svc.wikimedia.cloud:3306 -N &
+ssh magnus@login.toolforge.org -L 3333:oxztsldqokc.svc.trove.eqiad1.wikimedia.cloud:3306 -N &
